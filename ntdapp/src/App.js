6491aa1ff4e9c84a584e6804334be1d0522be152
Aasteri <samadewale2014@gmail.com>
2024-03-20 16:20:01 +0100

    added template page

--- ntdapp/src/App.js
+++ ntdapp/src/App.js
@@ -6,17 +6,24 @@ import About from "./Pages/About";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import "./App.css";
+import MapTemp from "./Pages/Templates/Map_Template";
+import TableTemp from "./Pages/Templates/Table_Template";
+import ChartTemp from "./Pages/Templates/Chart_Template";
+
 function App() {
   return (
     <>
       <div className="main">
-        <Navbar/>
+        <Navbar />
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/charttemp" element={<ChartTemp />}></Route>
+          <Route path="/maptemp" element={<MapTemp />}></Route>
+          <Route path="/tabletemp" element={<TableTemp />}></Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
---
+++ ntdapp/src/Pages/Templates/Map_Template.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MapTemp() {
+    return (
+        <div>MapTemp</div>
+    )
+}
