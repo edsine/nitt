758089ff671930fef2b06323bfbf826b5e113e12
Aasteri <samadewale2014@gmail.com>
2024-03-21 12:30:00 +0100

    chart ADDED MORE

--- ntdapp/src/Components/apexcharts/Apexhor.js
+++ ntdapp/src/Components/apexcharts/Apexhor.js
@@ -2,51 +2,51 @@ import React, { ReactDOM } from "react";
 import ReactApexChart from "react-apexcharts";
 
 class Apexhor extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-      
-        series: [{
-          data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
-        }],
-        options: {
-          chart: {
-            type: 'bar',
-            height: 350
-          },
-          plotOptions: {
-            bar: {
-              borderRadius: 4,
-              horizontal: true,
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          xaxis: {
-            categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
-              'United States', 'China', 'Germany'
-            ],
+    this.state = {
+
+      series: [{
+        data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+      }],
+      options: {
+        chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 4,
+            horizontal: true,
           }
         },
-      
-      
-      };
-    }
-
-  
-
-    render() {
-      return (
-        
-
-  <div id="chart">
-<ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
-</div>
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+            'United States', 'China', 'Germany'
+          ],
+        }
+      },
 
 
-      );
-    }
+    };
   }
-  export default Apexhor;
+
+
+
+  render() {
+    return (
+
+
+      <div id="chart">
+        <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
+      </div>
+
+
+    );
+  }
+}
+export default Apexhor;
